8a3fe5947fc628770867c4acc72e8361a8f0bc4c
volro7735 <vlad7735@gmail.com>
2026-09-19 22:29:21 +0100

    Add user input for name, age, and favourite colour

---
+++ week02/mitting.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("How old are you?")
+colour = input("What is your favourite colour?")
+print(f"Hi,my name is {name}. I'm {age} years old and my favorite colour is {colour}.") 
