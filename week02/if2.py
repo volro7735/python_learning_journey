462dd6f09e00298dd1e7282631e22cd3c58b4674
volro7735 <vlad7735@gmail.com>
2026-09-19 22:27:14 +0100

    Add age check for working age in if2.py

---
+++ week02/if2.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))
+print(name)
+if age >=18 and age <=65:
+    print("You are working age.")
+else:
+    print("You are not working age.")
