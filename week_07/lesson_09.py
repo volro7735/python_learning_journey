0f03f8b8bca912c568a1e35b162d5aec8632da50
volro7735 <vlad7735@gmail.com>
2026-09-20 20:52:08 +0100

    Add greeting logic based on user input

---
+++ week_07/lesson_09.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ").lower()
+if name == "admin":
+    print("Hello, boss! ")
+elif name == "guest":
+    print("Hello, guest! ")
+else:
+    print("Hello, stranger! ")
